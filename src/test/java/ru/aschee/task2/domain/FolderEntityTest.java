06b9b66170cc8d63916c642c74292610632ac74a
PistonAschee <faustplip@mail.ru>
2019-10-16 02:17:44 +0300

    add mockito-test

---
+++ src/test/java/ru/aschee/task2/domain/FolderEntityTest.java
@@ -0,0 +1,22 @@
+package ru.aschee.task2.domain;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SpringBootTest
+public class EntityTest {
+
+@Rule
+public ExpectedException thrown = 
+
+    @Test
+    public void create_new_node_and_get_correct_value() {
+    }
+}
